2418a4e85d1b0afeeacac67c99e03bab481f0281
RezendeLucas01 <contato.rezendelucas@gmail.com>
2024-08-28 11:02:13 -0300

    Update questions.sql

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -8,9 +8,18 @@
 * that are from either Japan or Australia
 */
 
+Select age, income, country from customers
+where ((age <= 30 or age >= 50) and income > 50000)
+or (age = 50 and (country = 'Japan' or country = 'Australia'))
+
 /*
 * DB: Store
 * Table: Orders
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+
+Select sum(netamount) from orders
+where (orderdate >= '2004-06-01' and orderdate <= '2004-06-30') and netamount > 100;
+
+result = 188060.29
